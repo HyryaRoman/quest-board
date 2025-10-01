8477c74aa1efa823648fdd06182e3984c5aeec5b
Hyria Roman <hyrya.roman@gmail.com>
2025-10-01 12:54:05 +0300

    docs: add comments to tests

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -4,24 +4,38 @@ import Page from '@/app/page'
 
 describe('Home page', () => {
   it('renders a header', () => {
+    // [Arrange] On the rendered page,
     render(<Page />)
 
+    // [Act] with no action required
+    
+    // [Assert] header should be present and visible
     const header = screen.getByRole('banner')
-
     expect(header).toBeInTheDocument()
+    expect(header).toBeVisible()
   })
+
   it('renders main content', () => {
+    // [Arrange] On the rendered page,
     render(<Page />)
 
+    // [Act] with no action required
+    
+    // [Assert] main content should be present and visible
     const main = screen.getByRole('main')
-
     expect(main).toBeInTheDocument()
+    expect(main).toBeVisible()
   })
+
   it('renders a footer', () => {
+    // [Arrange] On the rendered page,
     render(<Page />)
 
+    // [Act] with no action required
+    
+    // [Assert] footer should be present and visible
     const footer = screen.getByRole('contentinfo')
-
     expect(footer).toBeInTheDocument()
+    expect(footer).toBeVisible()
   })
 })
