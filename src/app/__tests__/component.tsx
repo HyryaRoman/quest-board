e101d990a730f87c64f0b9ea540e6df38bd4203a
Hyria Roman <hyrya.roman@gmail.com>
2025-10-04 11:15:41 +0300

    feat: add component test for main page

--- src/app/__tests__/component.tsx
+++ src/app/__tests__/component.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
-import Home from '../page'
+import Page from '../page'
 
 describe('<Home />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Home />)
+  it('renders header', () => {
+    // [Arrange] On home page,
+    const home = <Page />
+
+    // [Act] when it renders,
+    cy.mount(home)
+
+    // [Assert] the header is visible
+    cy.get('header').should('be.visible')
+  })
+
+  it('renders main content', () => {
+    // [Arrange] On home page,
+    const home = <Page />
+
+    // [Act] when it renders,
+    cy.mount(home)
+
+    // [Assert] the main content is visible
+    cy.get('main').should('be.visible')
+  })
+
+  it('renders footer', () => {
+    // [Arrange] On home page,
+    const home = <Page />
+
+    // [Act] when it renders,
+    cy.mount(home)
+
+    // [Assert] the footer is visible
+    cy.get('footer').should('be.visible')
   })
 })
